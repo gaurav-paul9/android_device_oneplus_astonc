c2caa135339409da7e1412cf3ae253101f180646
gaurav-paul9 <paulgaurav9@gmail.com>
2026-04-11 05:28:03 +0000

    astonc: Initial AxionOS Bringup

--- lineage_astonc.mk
+++ lineage_astonc.mk
@@ -13,6 +13,36 @@ $(call inherit-product, device/oneplus/astonc/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# AxionOS-Specific Flags
+AXION_MAINTAINER := Gaurav_Paul
+AXION_PROCESSOR := Snapdragon_8_Gen_2
+PRODUCT_NO_CAMERA:= false
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_DISABLE_EPPE := true
+TARGET_INCLUDES_LOS_PREBUILTS := true
+TARGET_PREBUILT_BCR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_AXFX := true
+
+# Define rear camera specs
+AXION_CAMERA_REAR_INFO := 50,8,2
+AXION_CAMERA_FRONT_INFO := 16
+
+# Flashlght_strength
+TORCH_STR_SUPPORTED := true
+
+# Charging
+BYPASS_CHARGE_SUPPORTED ?= true
+BYPASS_CHARGE_TOGGLE_PATH ?= /sys/class/power_supply/battery/input_suspend
+BYPASS_CHARGE_LEVEL_PATH ?= /sys/devices/platform/google,charger/charge_stop_level
+
+# Blur
+TARGET_ENABLE_BLUR := true
+
+#ScrollOptimizer
+persist.sys.perf.scroll_opt = true
+persist.sys.perf.scroll_opt.heavy_app = 2
+
 PRODUCT_NAME := lineage_astonc
 PRODUCT_DEVICE := astonc
 PRODUCT_MANUFACTURER := OnePlus
