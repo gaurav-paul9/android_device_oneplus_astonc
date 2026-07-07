9fa7e13b17e876e345ebf024d7310855b6afc686
gaurav-paul9 <paulgaurav9@gmail.com>
2026-07-07 16:52:10 +0530

    aston: Drop DeviceSettings from device.mk (now in common tree)

--- device.mk
+++ device.mk
@@ -54,10 +54,6 @@ PRODUCT_PACKAGES += \
     OPlusSettingsResTarget \
     OPlusSystemUIResTarget
 
-# Device Settings
-PRODUCT_PACKAGES += \
-    DeviceSettings
-
 # Power
 $(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libperfmgr-ext-astonc)
 
