443c1044672b9c292f7235002a5146709145e84b
gaurav-paul9 <paulgaurav9@gmail.com>
2026-07-15 18:17:21 +0530

    astonc: Include DeviceSettings in PRODUCT_PACKAGES

--- device.mk
+++ device.mk
@@ -54,6 +54,10 @@ PRODUCT_PACKAGES += \
     OPlusSettingsResTarget \
     OPlusSystemUIResTarget
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Power
 $(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libperfmgr-ext-astonc)
 
